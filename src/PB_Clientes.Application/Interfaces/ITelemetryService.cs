d3c73a753c69b00d02f54b10e50bbf135d4942a6
DinisSimoes <di.f.simoes@gmail.com>
2025-10-30 09:56:59 -0300

    add interface

---
+++ src/PB_Clientes.Application/Interfaces/ITelemetryService.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace PB_Clientes.Application.Interfaces
+{
+    public interface ITelemetryService
+    {
+        Activity? StartActivity(string name, ActivityKind kind = ActivityKind.Internal, ActivityContext? parentContext = null, IDictionary<string, object?>? tags = null);
+        void RestoreBaggage(Activity? activity, string? baggageJson);
+        void InjectTraceContext<T>(T context, Activity? activity) where T : class;
+    }
+}
