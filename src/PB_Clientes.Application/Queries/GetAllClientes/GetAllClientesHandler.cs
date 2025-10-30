1ac26625859a0dcb3d3b87d2b873dd76a30458b3
DinisSimoes <di.f.simoes@gmail.com>
2025-10-30 09:56:39 -0300

    add get all clientes

---
+++ src/PB_Clientes.Application/Queries/GetAllClientes/GetAllClientesHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using PB_Clientes.Application.Dtos;
+using PB_Clientes.Domain.Repositories;
+
+namespace PB_Clientes.Application.Queries.GetAllClientes
+{
+    public class GetAllClientesHandler : IRequestHandler<GetAllClientesQuery, IEnumerable<ClienteDto>>
+    {
+        private readonly IClienteRepository _repo;
+        public GetAllClientesHandler(IClienteRepository repo) => _repo = repo;
+
+        public async Task<IEnumerable<ClienteDto>> Handle(GetAllClientesQuery request, CancellationToken cancellationToken)
+        {
+            var clientes = await _repo.GetAllAsync(cancellationToken);
+            return clientes.Select(c => new ClienteDto(c.Id, c.Nome, c.Cpf, c.DataCriacao));
+        }
+    }
+}
---
+++ src/PB_Clientes.Application/Queries/GetAllClientes/GetAllClientesQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using PB_Clientes.Application.Dtos;
+
+namespace PB_Clientes.Application.Queries.GetAllClientes
+{
+    public record GetAllClientesQuery() : IRequest<IEnumerable<ClienteDto>>;
+}
