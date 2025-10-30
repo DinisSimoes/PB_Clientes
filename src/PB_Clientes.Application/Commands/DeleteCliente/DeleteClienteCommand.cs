48fd9f4db730a9338ca0b810b3e03e1e01e6adaa
DinisSimoes <di.f.simoes@gmail.com>
2025-10-30 09:55:44 -0300

    add delete cliente

---
+++ src/PB_Clientes.Application/Commands/DeleteCliente/DeleteClienteCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace PB_Clientes.Application.Commands.DeleteCliente
+{
+    public record DeleteClienteCommand(Guid Id) : IRequest<Unit>;
+}
---
+++ src/PB_Clientes.Application/Commands/DeleteCliente/DeleteClienteHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using PB_Clientes.Domain.Repositories;
+
+namespace PB_Clientes.Application.Commands.DeleteCliente
+{
+    public class DeleteClienteHandler : IRequestHandler<DeleteClienteCommand, Unit>
+    {
+        private readonly IClienteRepository _repo;
+        public DeleteClienteHandler(IClienteRepository repo) => _repo = repo;
+
+        public async Task<Unit> Handle(DeleteClienteCommand request, CancellationToken cancellationToken)
+        {
+            if (!await _repo.ExistsAsync(request.Id, cancellationToken))
+                return Unit.Value;
+
+            await _repo.DeleteAsync(request.Id, cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
